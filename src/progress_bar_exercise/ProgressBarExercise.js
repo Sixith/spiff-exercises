ebb409ed3cc59063ede1943fbb8aae208ffced91
John Walsh <jwjwalsh@gmail.com>
2021-08-07 22:17:22 -0500

    progress-bar-v1: Committing aborted timer code.

--- src/progress_bar_exercise/ProgressBarExercise.js
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Exercise from "../exercise/Exercise";
+import ProgressBar from "./ProgressBar";
+
+import "./ProgressBarExercise.scss"
 
 const ProgressBarExercise = () => {
   return (
@@ -18,5 +21,65 @@ export default ProgressBarExercise;
 // ----------------------------------------------------------------------------------
 
 const Solution = () => {
-  return <div>Add solution here</div>;
+  const [progress, setProgress] = React.useState(0);
+  const [visibility, setProgress] = React.useState("visible");
+  const [opacity, setProgress] = React.useState(1);
+  const [progressTransition, setProgress] = React.useState(0);
+  const [visibilityTransition, setProgress] = React.useState(0);
+  const [opacityTransition, setProgress] = React.useState(0);
+
+  const startRequest = () => {
+    console.log("starting");
+    setProgress(0);
+    fakeRequest(90,15000);
+  }
+  
+  const finishRequest = () => {
+    console.log("finishing");
+    clearInterval(timer);
+    setProgress(100);
+  }
+  
+  const startButtonText = () => {
+    if(progress === 0 || progress === 100) {
+      return "Start Request";
+    } else {
+      return "Loading...";
+    }
+  }
+
+  const fakeRequest = (targetPercentage, durationInMS) => {
+    let interval = 1000;
+    let startPercentage = progress;
+    let incrementPercentage = (targetPercentage - startPercentage) * (interval/durationInMS);
+    console.log("increment", incrementPercentage);
+    timer = setInterval(() => {
+        console.log("timinggggg: "+progress);
+        /*let newProgress = progress+incrementPercentage;
+        console.log("newprogress",newProgress);
+        if(newProgress >= targetPercentage ) {
+          console.log("doneeee");
+          setProgress(targetPercentage);
+          clearInterval(timer);
+        } else {
+          console.log("increment");
+          setProgress(newProgress);
+        }*/
+        incrementProgress(1);
+      }, 1000);
+  }
+
+  const incrementProgress = (incrementPercentage, delay) => {
+    console.log("incrementing");
+    setProgress(progress+incrementPercentage);
+    //setTimeout(incrementProgress(incrementPercentage, delay), delay);
+  }
+  
+  return <div>
+    <ProgressBar progress={progress} />
+    <button className="start-button" onClick={ startRequest }>{ startButtonText() }</button>
+    <button className="finish-button" onClick={ finishRequest }>Finish Request</button>
+  </div>;
 };
+
+
